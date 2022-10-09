b3212195479c392e8ab95dc4c42eefc4476669ee
CodeHntr <paveltrokhanovsky@gmail.com>
2022-10-09 18:54:22 +0300

    add up in migration order_product

--- database/migrations/2022_10_09_154735_order_product.php
+++ database/migrations/2022_10_09_154735_order_product.php
@@ -13,7 +13,18 @@ return new class extends Migration
      */
     public function up()
     {
-        //
+        Schema::create('order_product', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('order_id');
+            $table->integer('prod');
+            $table->integer('count');
+            $table->integer('price');
+            $table->integer('summa');
+
+            $table->foreign('owner_id')->references('id')->on('order');
+            $table->foreign('prod')->references('prod')->on('order');
+            $table->foreign('price')->references('price')->on('order');
+        });
     }
 
     /**
