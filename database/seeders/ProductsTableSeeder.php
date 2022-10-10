e42dc9346e7c2d51036374d6d91eca8ae9c6cc3e
CodeHntr <paveltrokhanovsky@gmail.com>
2022-10-10 16:57:51 +0300

    Add run function and initializate UsersSeedTable

--- database/seeders/ProductsTableSeeder.php
+++ database/seeders/ProductsTableSeeder.php
@@ -4,6 +4,7 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
 
 class ProductsTableSeeder extends Seeder
 {
@@ -14,6 +15,12 @@ class ProductsTableSeeder extends Seeder
      */
     public function run()
     {
-        //
+        DB::table('products')->insert([
+            'name' => str_random(10),
+            'category' => str_random(5),
+            'count' => 10,
+            'price' => '15.50',
+            'limit' => 3
+        ]);
     }
 }
