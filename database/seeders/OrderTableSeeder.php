fe0813701006d913ed80d9fe6593661c9a4430d9
CodeHntr <paveltrokhanovsky@gmail.com>
2022-10-10 19:38:34 +0300

    rename file

--- database/seeders/OrderTableSeeder.php
+++ database/seeders/OrderTableSeeder.php
@@ -5,7 +5,7 @@ namespace Database\Seeders;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
-class Order extends Seeder
+class OrderTableSeeder extends Seeder
 {
     /**
      * Run the database seeds.
