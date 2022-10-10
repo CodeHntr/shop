cdd997bca53606daf59d0ac4a669efddfc2de019
CodeHntr <paveltrokhanovsky@gmail.com>
2022-10-10 14:04:08 +0300

    add to Product model properties fillable and hidden

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -8,4 +8,17 @@ use Illuminate\Database\Eloquent\Model;
 class Product extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+        '',
+        'name',
+        'category',
+        'count',
+        'price',
+        'limit',
+    ];
+
+    protected $hidden = [
+
+    ];
 }
