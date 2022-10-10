3bd5f53f65965f50a2ffbb7f232b6e6d418d870d
CodeHntr <paveltrokhanovsky@gmail.com>
2022-10-10 16:41:28 +0300

    Create Products seed

--- app/Models/Order.php
+++ app/Models/Order.php
@@ -9,6 +9,12 @@ class Order extends Model
 {
     use HasFactory;
 
+    public const STATUS_NEW = 1;
+
+    public const STATUS_IN_PROGRESS = 2;
+
+    public const STATUS_READY = 3;
+
     protected $fillable = [
         'user_id',
         'status',
--- app/Models/Product.php
+++ app/Models/Product.php
@@ -10,7 +10,6 @@ class Product extends Model
     use HasFactory;
 
     protected $fillable = [
-        '',
         'name',
         'category',
         'count',
